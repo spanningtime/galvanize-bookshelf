c2a533e2aa100f0c4772a01f4f4445bbd6058212
William Knight <kniwilliam@gmail.com>
2016-06-28 10:06:52 -0700

    corrected typo in authors seed

---
+++ migrations/20160627230425_books.js
@@ -0,0 +1,23 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('books', (table) => {
+    table.increments();
+    table
+      .integer('author_id')
+      .notNullable()
+      .references('id')
+      .inTable('authors')
+      .onDelete('CASCADE')
+      .index();
+    table.string('title').notNullable().defaultTo('');
+    table.string('genre').notNullable().defaultTo('');
+    table.text('description').notNullable().defaultTo('');
+    table.text('cover_url').notNullable().defaultTo('');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('books')
+};
